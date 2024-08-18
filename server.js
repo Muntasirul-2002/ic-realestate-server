1fdab2c82192041db780b0f2e2f82f6411f04fbf
muntasirul <muntasirul.msd@gmail.com>
2024-08-19 00:09:44 +0530

    add cors origin

--- server.js
+++ server.js
@@ -1,44 +1,64 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import authRoute from './routes/authRoute.js';
-import propertyRoute from './routes/propertyRoute.js';
-import { connectDB } from './config/db.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoute from "./routes/authRoute.js";
+import propertyRoute from "./routes/propertyRoute.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
+
+//cors origin setup
+const allowOrigin = [
+    "http://localhost:8080",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    undefined,
+]
+const corsOptions = {
+    origin: (origin, callback)=>{
+        if(allowOrigin.includes(origin)){
+            callback(null, true)
+        }else{
+            callback(new Error(`url not allowed by CORS origin: ${origin}`))
+        }
+    },
+    credentials: true,
+}
 
 // Get the directory name in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Ensure the uploads directory exists
-const uploadsDir = path.join(__dirname, 'routes', 'uploads');
+const uploadsDir = path.join(__dirname, "routes", "uploads");
 if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, { recursive: true });
+  fs.mkdirSync(uploadsDir, { recursive: true });
 }
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions))
 app.use(express.json());
+
 
 // DB Connection
 connectDB();
 
 // Base Route
-app.get('/', (req, res) => {
-    res.send("Welcome to the myhome server");
+app.get("/", (req, res) => {
+  res.send("Welcome to the myhome server");
 });
 
 // Routers
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/property', propertyRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/property", propertyRoute);
 
 // Start the Server
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
